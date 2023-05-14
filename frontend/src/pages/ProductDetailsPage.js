96287fe477e43bea13ee26f921abb4324bbf63aa
Arvin <arvin.yuwono@binus.ac.id>
2023-05-14 20:52:51 +0700

    Update ProductDetailsPage.js

--- frontend/src/pages/ProductDetailsPage.js
+++ frontend/src/pages/ProductDetailsPage.js
@@ -2,9 +2,24 @@ import React from "react";
 import { useParams } from "react-router-dom";
 
 const ProductDetailsPage = () => {
-  const { id } = useParams();
-  console.log(id);
-  return <div>ProductDetailsPage</div>;
+  return (
+    <Row className='mt-5'>
+      <Col md={4}>Image</Col>
+      <Col md={8}>
+        <Row>
+          <Col md={8}>book title,price,description,rating</Col>
+          <Col md={4}>book status,quantity</Col>
+        </Row>
+        <Row>
+          <Col className='mt-5'>
+            <h5>Reviews</h5>
+          </Col>
+          <hr />
+          give a review
+        </Row>
+      </Col>
+    </Row>
+  );
 };
 
 export default ProductDetailsPage;
