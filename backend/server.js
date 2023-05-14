b6634e7f396a5102f03f4d48fb4192b70dd88d93
Arvin <arvin.yuwono@binus.ac.id>
2023-05-14 23:14:54 +0700

    Revert "Prepare API"
    This reverts commit bd0d8533b675771d6cf306e461aac540801f8de6.

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,10 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-const apiRoutes = require("./routes/apiRoutes");
 
 app.get("/", (req, res) => {
-  res.json({ message: "API running..." });
+  res.send("Hello World!");
 });
-
-app.get("/api", apiRoutes);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
