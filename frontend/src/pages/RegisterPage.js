1d54dd46d6800b07bb1070771be648fabc7848ee
Arvin <arvin.yuwono@binus.ac.id>
2023-05-21 07:59:17 +0700

    Finish Register Page (frontend)

--- frontend/src/pages/RegisterPage.js
+++ frontend/src/pages/RegisterPage.js
@@ -1,6 +1,17 @@
 import React from "react";
-import { Container, Row, Col, Form, Button, InputGroup } from "react-bootstrap";
+import {
+  Container,
+  Row,
+  Col,
+  Form,
+  Button,
+  InputGroup,
+  Spinner,
+  Alert,
+} from "react-bootstrap";
 import { useState } from "react";
+import { Link } from "react-router-dom";
+
 const RegisterPage = () => {
   const [validated, setValidated] = useState(false);
 
@@ -19,26 +30,19 @@ const RegisterPage = () => {
         <Col md={6}>
           <h1>Register</h1>
           <Form noValidate validated={validated} onSubmit={handleSubmit}>
-            <Form.Group as={Col} md='4' controlId='validationCustom01'>
-              <Form.Label>First name</Form.Label>
+            <Form.Group className='mb-3' controlId='validationEmailAddress'>
+              <Form.Label>Email Address</Form.Label>
               <Form.Control
                 required
-                type='text'
-                placeholder='First name'
-                defaultValue='Mark'
+                type='email'
+                placeholder='myemail@example.com'
               />
               <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+              <Form.Control.Feedback type='invalid'>
+                Please enter a valid email address
+              </Form.Control.Feedback>
             </Form.Group>
-            <Form.Group as={Col} md='4' controlId='validationCustom02'>
-              <Form.Label>Last name</Form.Label>
-              <Form.Control
-                required
-                type='text'
-                placeholder='Last name'
-                defaultValue='Otto'
-              />
-              <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-            </Form.Group>
+
             <Form.Group as={Col} md='4' controlId='validationCustomUsername'>
               <Form.Label>Username</Form.Label>
               <InputGroup hasValidation>
@@ -50,32 +54,37 @@ const RegisterPage = () => {
                   required
                 />
                 <Form.Control.Feedback type='invalid'>
-                  Please choose a username.
+                  Please enter a username
                 </Form.Control.Feedback>
               </InputGroup>
             </Form.Group>
 
-            <Form.Group as={Col} md='6' controlId='validationCustom03'>
-              <Form.Label>City</Form.Label>
-              <Form.Control type='text' placeholder='City' required />
-              <Form.Control.Feedback type='invalid'>
-                Please provide a valid city.
-              </Form.Control.Feedback>
+            <Form.Group as={Col} md='4' controlId='validationPassword'>
+              <Form.Label>Password</Form.Label>
+              <InputGroup hasValidation>
+                <Form.Control
+                  type='password'
+                  placeholder='Password'
+                  aria-describedby='inputGroupPrepend'
+                  minLength={6}
+                  required
+                />
+                <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+                <Form.Control.Feedback type='invalid'>
+                  Please enter a valid password
+                </Form.Control.Feedback>
+              </InputGroup>
+              <Form.Text className='text-muted'>
+                At least 6 characters required
+              </Form.Text>
             </Form.Group>
-            <Form.Group as={Col} md='3' controlId='validationCustom04'>
-              <Form.Label>State</Form.Label>
-              <Form.Control type='text' placeholder='State' required />
-              <Form.Control.Feedback type='invalid'>
-                Please provide a valid state.
-              </Form.Control.Feedback>
-            </Form.Group>
-            <Form.Group as={Col} md='3' controlId='validationCustom05'>
-              <Form.Label>Zip</Form.Label>
-              <Form.Control type='text' placeholder='Zip' required />
-              <Form.Control.Feedback type='invalid'>
-                Please provide a valid zip.
-              </Form.Control.Feedback>
-            </Form.Group>
+
+            <Row>
+              <Col>
+                Already have an account?
+                <Link to={"/login"}>Login</Link>
+              </Col>
+            </Row>
 
             <Form.Group className='mb-3'>
               <Form.Check
@@ -85,7 +94,22 @@ const RegisterPage = () => {
                 feedbackType='invalid'
               />
             </Form.Group>
-            <Button type='submit'>Submit form</Button>
+            <Button type='submit'>
+              <Spinner
+                as='span'
+                animation='border'
+                size='sm'
+                role='status'
+                aria-hidden='true'
+              />
+              Register
+            </Button>
+            <Alert show={true} variant='danger'>
+              Email has already been used
+            </Alert>
+            <Alert show={true} variant='info'>
+              Account successfully registered
+            </Alert>
           </Form>
         </Col>
       </Row>
